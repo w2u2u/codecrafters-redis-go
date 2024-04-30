d6cc37138df69d890d50f4a657da89e0415229b4
Warunyu Jaimuang <warunyu.pon@gmail.com>
2024-05-01 00:04:17 +0700

    pass stage

--- app/server.go
+++ app/server.go
@@ -14,9 +14,25 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error read request from client: ", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("Server received message: ", string(buf[:n]))
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		fmt.Println("Error write response to client: ", err.Error())
 		os.Exit(1)
 	}
 }
